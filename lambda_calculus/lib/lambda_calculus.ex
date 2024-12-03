fcf7c30a31cb35ead1f7027c9545f8f02b08e5f4
Every <christiansantoslima21@gmail.com>
2024-12-03 15:13:50 -0300

    build: add project structure

---
+++ lambda_calculus/lib/lambda_calculus.ex
@@ -0,0 +1,18 @@
+defmodule LambdaCalculus do
+  @moduledoc """
+  Documentation for `LambdaCalculus`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LambdaCalculus.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
